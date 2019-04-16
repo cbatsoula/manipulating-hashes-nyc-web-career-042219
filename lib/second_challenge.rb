d04fd2f0fa331b800c7245fb396d50fab211a39d
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-16 03:59:31 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -9,6 +9,10 @@ def second_challenge
   #code your solution here!
 nuhash = {}
 groceries.values 
-groceries.values 
+ 
+ holiday_supplies[:winter].map do |holiday, supplies|
+supplies
+end.flatten
+end
 
 end
